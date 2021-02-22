d0248028d235b40cf99e0a66fb0c3326c3d67587
Oscar Granados <ojgranadosg@gmail.com>
2021-02-22 17:42:02 -0500

    Agrego chart.js y muestro valores globales en Pie de Chart.js

--- src/components/Graphics/Graphics.js
+++ src/components/Graphics/Graphics.js
@@ -1,20 +1,36 @@
-import React from 'react';
-//import {DataCountryContext} from '../../context/DataCountryContext';
-
-
+import React, {useContext} from 'react';
+import { Pie } from 'react-chartjs-2';
+import { ValuesContext } from "../../context/ValuesContext";
 
 const Graphics = () => {
 
-    //const {setdataform, setconsult, dataCountry} = useContext(DataCountryContext);
+    const { valueTotal } = useContext(ValuesContext);
 
     return (  
-        <div>
-           <h1>Graficas</h1>
-           
-            
-
-        </div>
-        
+           <Pie 
+                data={{
+                    labels: ['CONFIRMADOS', 'MUERTES', 'RECUPERADOS'],
+                    datasets : [
+                        {
+                            label: 'Casos totales',
+                            data: [valueTotal.Global.TotalConfirmed, valueTotal.Global.TotalDeaths, valueTotal.Global.TotalRecovered],
+                            backgroundColor: [
+                                'rgba(255, 159, 64, 0.2)',
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(75, 192, 192, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 159, 64, 1)',
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(75, 192, 192, 1)'
+                            ],
+                            borderWidth: 3
+                        }
+                    ]
+                }}
+                height={450}
+                width={660}    
+           />         
     );
 }
  
